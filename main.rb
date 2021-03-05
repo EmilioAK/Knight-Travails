30d6572722fa7bb9053143f86e1b8d279a6dc313
EmilioAK <emilio.alvarez96@gmail.com>
2021-03-05 22:36:41 +0100

    First working version

--- main.rb
+++ main.rb
@@ -1,28 +1,29 @@
-require 'pp'
-
 class KnightTravails
-  attr_accessor :all_moves_h
- 
   def initialize(start, stop)
     @start = start
     @stop = stop
     @board = make_board
     @all_moves_h = all_moves
+    @path_map = {}
   end
 
-  def least_moves_to_stop
-    pp grade_board
-    pp get_score(@stop)
+  def knight_moves
+    grade_board
+    path = @path_map[@stop]
+    puts "You made it in #{path} moves! Here's your path:"
+    path.each { |move| p move }
   end
+
+  private
 
   def grade_board(current = @start, score = 0, path = [])
-    # Path doesn't currently get the shortest path
-    if get_score(current) < score
-      return
-    else
-      change_score(current, score)
-      get_moves(current).each {|move| grade_board(move, score + 1, path)}
-    end
+    return if get_score(current) < score
+
+    change_score(current, score)
+    p = path.clone
+    p << current
+    @path_map[current] = p
+    get_moves(current).each { |move| grade_board(move, score + 1, p) }
   end
 
   def get_moves(square)
@@ -39,19 +40,17 @@ class KnightTravails
   end
 
   def get_square(sub_name)
-    @all_moves_h.keys.select {|e| e[0..1] == sub_name}.flatten
+    @all_moves_h.keys.select { |e| e[0..1] == sub_name }.flatten
   end
 
-  private
-
   def valid_moves(start)
-    @board.select {|stop| valid_move?(start, stop)}
+    @board.select { |stop| valid_move?(start, stop) }
   end
 
   def valid_knight_movement?(start, stop)
     valid_y_movement = (start[0] - stop[0]).abs == 2 && (start[1] - stop[1]).abs == 1
     valid_x_movement = (start[1] - stop[1]).abs == 2 && (start[0] - stop[0]).abs == 1
-    
+
     valid_y_movement || valid_x_movement
   end
 
@@ -66,13 +65,9 @@ class KnightTravails
   end
 
   def all_moves
-    infinity = 1/0.0
-    moves = @board.map {|start| valid_moves(start)}
+    infinity = 1 / 0.0
+    moves = @board.map { |start| valid_moves(start) }
     board_hash = Hash[@board.zip moves]
-    board_hash.transform_keys {|k| k + [infinity]}
+    board_hash.transform_keys { |k| k + [infinity] }
   end
 end
-
-bajs = KnightTravails.new([4,4], [4,2])
-
-pp bajs.grade_board
